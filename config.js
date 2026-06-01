8471bdb94d7e29da9d5e7533cbdec52ec83d5a22
Monner9i231 <108022459+Monner9i231@users.noreply.github.com>
2026-06-01 02:25:39 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 // Få gratis token på: https://www.mapillary.com/developer
 
 const CONFIG = {
-  mapillaryToken: "MLY|26954640690895103|698546da8f7f251ed5c5c7bb8192465e",
+  mapillaryToken: "DIN_TOKEN_HER",
 };
